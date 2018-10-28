da93e252ea1ae7250fc90ccffb007f0fcf256dbc
mehmet.kozan <mehmet.kozan@zirveyazilim.net>
2018-10-28 12:34:01 +0300

    backward compatibility added.

--- NOTES.md
+++ NOTES.md
@@ -3,3 +3,4 @@
 //https://github.com/mozilla/pdf.js/issues/2140
 //https://gist.github.com/hubgit/600ec0c224481e910d2a0f883a7b98e3
 //https://gist.github.com/hubgit/600ec0c224481e910d2a0f883a7b98e3
+//https://github.com/mozilla/pdf.js/issues/6634 
--- index.js
+++ index.js
@@ -1,27 +1,69 @@
-import { readFileSync, writeFileSync } from 'fs';
-import PDF from './lib/pdf-parse.js';
+const pdf = require('./lib/pdf-parse.js');
 
-export default PDF;
+module.exports = pdf;
 
-let isDebugMode = !module.parent; 
+let isDebugMode = ! module.parent;
 
 //process.env.AUTO_KENT_DEBUG
-
-
 //for testing purpose
 if (isDebugMode) {
+    const FS = require('fs');
+    let pdfPath = './test/data/05-versions-space.pdf';
+    let dataBuffer = FS.readFileSync(pdfPath);
 
-    PDF.Options = {
+    pdf.options = {
         max: 0,
-        version: 'v1.10.100',
+        version: 'default',
         normalizeWhitespace: false,
-        disableCombineTextItems: false
+        disableCombineTextItems: false,
+        disableWorker: true,
+        verbosity: -1
     };
 
-    PDF.Custom(dataBuffer || path,function customPageRenderer(pageTextContent){
+    let customPageRenderer = function (pageContent){
+        let currentPageText = "";
+        let currentYPosition = null;
+        let lastYPosition = null;
+
+        for (let item of pageContent.items) {
+            currentYPosition = item.transform[5];
+    
+            if (lastYPosition == currentYPosition || lastYPosition == null){
+                currentPageText = `${currentPageText}${item.str}`;
+            }  
+            else{
+                currentPageText =`${currentPageText}\n${item.str}`;
+            } 
+            lastYPosition = currentYPosition;
+        }
+        return currentPageText;
+    }
+
+    pdf.custom(dataBuffer,customPageRenderer).then((custom)=>{
+        
+    });
+    
+    pdf.text(dataBuffer).then((text)=>{
 
     });
 
+    pdf.info(dataBuffer).then((info)=>{
+
+    });
+
+    pdf.table(dataBuffer).then((table)=>{
+
+    });
+
+    pdf.xml(dataBuffer).then((table)=>{
+
+    });
+
+    pdf.json(dataBuffer).then((table)=>{
+
+    });
+
+    /*
     PDF.Text(dataBuffer || path).then((result)=>{
         
     });
@@ -51,5 +93,6 @@ if (isDebugMode) {
     }).catch(function(err) {
         debugger;
     });
+    */
 
 }
